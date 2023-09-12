0fa624bc6d2016c8893a225cb9f27f2ef588587b
Hanan Adipratama <hananadipratama21@gmail.com>
2023-09-13 00:09:05 +0700

    albion storage blueprint

--- main/models.py
+++ main/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    date_added = models.DateField(auto_now_add=True)
-    price = models.IntegerField()
+    accountID = models.CharField(max_length=255)
+    balance = models.IntegerField()
+    # date_added = models.DateField(auto_now_add=True)
+    item = models.CharField(max_length=255)
     description = models.TextField()
+    itemPower = models.IntegerField()
+    tier = models.IntegerField()
+    amount = models.IntegerField()
+    place = models.CharField(max_length=255)
+    price = models.IntegerField()
--- main/views.py
+++ main/views.py
@@ -3,8 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Hanan Adipratama',
-        'class': 'PBP B'
+        'accountID': 'RigenMengaji',
+        'balance' : '15000000',
+        'item' : 'Cursed Staff',
+        'description': 'The Adept\'s Great Cursed Staff is a Tier 4 Cursed Staff which may be obtained by crafting or via the Market Place',
+        'itemPower' : '800',
+        'tier' : '4',
+        'amount' : '1',
+        'place' : 'Lymhurst Bank',
+        'price' : '2000'
+
+
     }
 
     return render(request, "main.html", context)
