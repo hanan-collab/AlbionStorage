81ffd65989943a6dfa5fdbe5edb60033d76291dd
hanan-collab <63461469+hanan-collab@users.noreply.github.com>
2023-09-13 09:27:41 +0700

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,13 +1,7 @@
 from django.db import models
 
 class Product(models.Model):
-    accountID = models.CharField(max_length=255)
-    balance = models.IntegerField()
-    # date_added = models.DateField(auto_now_add=True)
-    item = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    date_added = models.DateField(auto_now_add=True)
     description = models.TextField()
-    itemPower = models.IntegerField()
-    tier = models.IntegerField()
-    amount = models.IntegerField()
-    place = models.CharField(max_length=255)
     price = models.IntegerField()
